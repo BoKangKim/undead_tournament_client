674e3a96ea8cee57c80aea326af7f8da6f04ccb4
BoKangKim <bkbk0329@naver.ocm>
2025-04-09 20:58:12 +0900

    WIP : Add Unit Data

---
+++ Assets/Game/01.Script/Data/UnitData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Data
+{
+    public class UnitData
+    {
+        private int hp;
+        private int mana;
+
+        public int HP => hp;
+        public int Mana => mana;
+    }
+}
--- Assets/Game/01.Script/Player/PlayerController.cs
+++ Assets/Game/01.Script/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Game.Card;
+using Game.Data;
 using UnityEngine;
 
 namespace Game.Player
@@ -8,9 +9,14 @@ namespace Game.Player
     // 유닛 데이터, 카드 데이터 관리
     public class PlayerController : MonoBehaviour
     {
+        // View
         [SerializeField] private Unit unit = null;
+        // Data
+        private UnitData unitData = null;
+        // Card (Logic)
         [SerializeField] private CardBase[] pickedCards; //= new CardBase[3];
         [SerializeField] private int testIndex = 0;
+
         private (int x, int y) curCell = (0, 1);
         public (int x, int y) CurCell => curCell;
 
